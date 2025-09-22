57a43c6982c8d0100500021a5e3b0973ac959452
Portatil D <david.maldonado24@estudiantes.uva.es>
2025-09-22 21:02:30 +0200

    Justo antes de DOM

--- javascript/script.js
+++ javascript/script.js
@@ -76,5 +76,40 @@ function areaRectangulo(base) {
     return base * altura;
 }
 console.log(areaRectangulo(5));
-console.log(altura); // Error: altura no está definida fuera de la función
+/*console.log(altura);  //Error: altura no está definida fuera de la función*/
 
+/*Objetos*/
+let persona = {
+    nombre: "Juan",
+    apellido: "Pérez",
+    edad: 30,
+    ciudad: "Madrid",
+    
+    nombrecompleto: function() {
+        return this.nombre + " " + this.apellido;
+    }
+};
+
+console.log("Nombre: " + persona.nombre);
+console.log("Edad: " + persona.edad);
+console.log("Ciudad: " + persona.ciudad);
+console.log("Nombre completo: " + persona.nombrecompleto());
+
+/*Arrays*/
+let frutas = ["Manzana", "Banana", "Cereza"];
+console.log("Frutas: " + frutas);
+console.log("Fruta 2: " + frutas[1]); // Accede al segundo elemento
+frutas[2] = "Fresa"; // Modifica el tercer elemento
+console.log("Frutas después de modificar: " + frutas);
+console.log("Número de frutas: " + frutas.length); // Longitud del array
+frutas.push("Naranja"); // Añade un elemento al final
+console.log("Frutas después de push: " + frutas);
+frutas.pop(); // Elimina el último elemento
+frutas.shift(); // Elimina el primer elemento
+frutas.unshift("Melocotón"); // Añade un elemento al principio
+console.log("Frutas después de pop, shift y unshift: " + frutas);
+
+const resultados = [55, 70, 85, 90, 100];
+for(let i=0; i < resultados.length; i++) {
+    console.log("Resultado: " + resultados[i]);
+}
